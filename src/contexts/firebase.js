5add278a305d7a6187eecc695cbd3462b79adec6
Mayank Singh <mayankayush173@gmail.com>
2021-05-04 02:18:35 +0530

    added request form

--- src/contexts/firebase.js
+++ src/contexts/firebase.js
@@ -1,57 +1,55 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
-import {UserRole} from "../utils"
+import { UserRole } from "../utils";
 
 const config = {
-    apiKey: "AIzaSyDv_S89GRCA5D3d304dZVEBTJPm8Ssj8h0",
-    authDomain: "covid-portal-b0e50.firebaseapp.com",
-    projectId: "covid-portal-b0e50",
-    storageBucket: "covid-portal-b0e50.appspot.com",
-    messagingSenderId: "877577916438",
-    appId: "1:877577916438:web:f2b4320ad3e87f16ba2537"
-  }
+  apiKey: "AIzaSyDv_S89GRCA5D3d304dZVEBTJPm8Ssj8h0",
+  authDomain: "covid-portal-b0e50.firebaseapp.com",
+  projectId: "covid-portal-b0e50",
+  storageBucket: "covid-portal-b0e50.appspot.com",
+  messagingSenderId: "877577916438",
+  appId: "1:877577916438:web:f2b4320ad3e87f16ba2537"
+};
 
-firebase.initializeApp(config)
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
-
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 export const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    provider.setCustomParameters({
-        hd: "smail.iitm.ac.in"
-    })
-    return auth.signInWithPopup(provider)
-}
-
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    hd: "smail.iitm.ac.in"
+  });
+  return auth.signInWithPopup(provider);
+};
 
 export const createUser = async (cred) => {
-    const {user} = cred
+  const { user } = cred;
 
   const userRef = firestore.doc(`users/${user.uid}`);
   const snapshot = await userRef.get();
 
-    if (snapshot.exists) return
-    
-    const { email, displayName, photoURL } = user;
-    try {
-        await userRef.set({
-            displayName,
-            email,
-            photoURL,
-            role: UserRole.Student,
-        });
-    } catch (error) {
-        console.error("Error creating user document", error);
-    }
-//     await firestore.collection("users").doc(cred.user.uid).set({
-//         name: cred.user.displayName,
-//         email: cred.user.email,
-//         
-//         profilePic: cred.user.photoURL
-//     })
-}
+  if (snapshot.exists) return;
+
+  const { email, displayName, photoURL } = user;
+  try {
+    await userRef.set({
+      displayName,
+      email,
+      photoURL,
+      role: UserRole.Student
+    });
+  } catch (error) {
+    console.error("Error creating user document", error);
+  }
+  //     await firestore.collection("users").doc(cred.user.uid).set({
+  //         name: cred.user.displayName,
+  //         email: cred.user.email,
+  //
+  //         profilePic: cred.user.photoURL
+  //     })
+};
 
 export const getUser = async (uid) => {
   if (!uid) return null;
@@ -60,21 +58,23 @@ export const getUser = async (uid) => {
   } catch (error) {
     console.error("Error fetching user", error);
   }
-}
-
+};
 
 export const getRequests = async () => {
-    const requestsRef =  await firestore.collection("requests").orderBy("createdAt").limit(10).get()
+  const requestsRef = await firestore
+    .collection("requests")
+    .orderBy("createdAt")
+    .limit(10)
+    .get();
 
-    return requestsRef.docs.map(req => req.data())
-
-}
+  return requestsRef.docs.map((req) => req.data());
+};
 
 // export const getMoreRequests = async ()=>{
 
 // }
 export const signOut = () => {
-    return auth.signOut()
-}
+  return auth.signOut();
+};
 
-export default firebase
+export default firebase;
--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,26 +1,24 @@
-import React, { useContext } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Home from "../components/Home"
-import SignIn from "../components/SignIn"
-import * as ROUTES from "../constants/routes"
-import {withAuthUserProvider} from "../contexts"
-import {RootLayout} from "../components/layouts"
-import NewRequest from "../components/newRequest"
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
+import SignIn from "../components/SignIn";
+import * as ROUTES from "../constants/routes";
+import { withAuthUserProvider } from "../contexts";
+import { RootLayout } from "../components/layouts";
+import NewRequest from "../components/newRequest";
 export default withAuthUserProvider(({ Component, pageProps }) => {
-    // const { authUser } = useContext(AuthUserContext)
+  // const { authUser } = useContext(AuthUserContext)
 
-    
-    return (
-        <Router>
-            <Switch>
-
-                <Route path={ROUTES.SIGNIN} component={SignIn} />
-                <RootLayout>
-                    <Route exact path={ROUTES.HOME} component={Home}></Route>
-                    {/* newRoutes go here */}
-                    <Route exact path="/newRequest" component={NewRequest}></Route>
-                </RootLayout>
-            </Switch>
-        </Router>
-    )
-})
+  return (
+    <Router>
+      <Switch>
+        {/* <Route path={ROUTES.SIGNIN} component={SignIn} /> */}
+        <RootLayout>
+          <Route exact path={ROUTES.HOME} component={Home}></Route>
+          {/* newRoutes go here */}
+          <Route exact path="/newRequest" component={NewRequest}></Route>
+        </RootLayout>
+      </Switch>
+    </Router>
+  );
+});
