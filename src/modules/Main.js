6fd2b3dc20653e9893d06e40919c2a3cba84935b
itssiva24 <sivaiitm24@gmail.com>
2021-05-04 12:59:44 +0530

    typo fixed

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,12 +1,12 @@
-mport React, { useContext } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Home from "../components/Home"
-import SignIn from "../components/SignIn"
-import * as ROUTES from "../constants/routes"
-import {withAuthUserProvider} from "../contexts"
-import {RootLayout} from "../components/layouts"
-import NewRequest from "../components/newRequest"
-import RequestDetail from "../components/RequestDetail"
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
+import SignIn from "../components/SignIn";
+import * as ROUTES from "../constants/routes";
+import { withAuthUserProvider } from "../contexts";
+import { RootLayout } from "../components/layouts";
+import NewRequest from "../components/newRequest";
+import RequestDetail from "../components/RequestDetail";
 export default withAuthUserProvider(({ Component, pageProps }) => {
     // const { authUser } = useContext(AuthUserContext)
 
@@ -32,7 +32,11 @@ export default withAuthUserProvider(({ Component, pageProps }) => {
                         path={ROUTES.REQUESTS_ASSIGNED}
                         component={() => <h2>Requests Assigned</h2>}
                     ></Route>
-                    <Route exact path={ROUTES.REQUEST_DETAIL} component={RequestDetail}></Route>
+                    <Route
+                        exact
+                        path={ROUTES.REQUEST_DETAIL}
+                        component={RequestDetail}
+                    ></Route>
                 </RootLayout>
             </Switch>
         </Router>
