aef5fa631f59dd635b06a158ece4437a66ef8e1a
Mayank Singh <mayankayush173@gmail.com>
2021-05-13 20:07:19 +0530

    UI change

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,43 +5,30 @@ import { withAuthorization } from "../../contexts";
 import { useHistory } from "react-router";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Link from "@material-ui/core/Link";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Deer from "../../assets/images/deer.jpg";
 import Logo from "../../assets/images/logo.png";
+import "./SignIn.css";
 
 const condition = (authUser) => !authUser;
 
 const useStyles = makeStyles((theme) => ({
     root: {
         height: "100vh",
-    },
-    image: {
+        display: "flex",
         backgroundImage: `url(${Deer})`,
-        backgroundRepeat: "no-repeat",
-        backgroundColor:
-            theme.palette.type === "light"
-                ? theme.palette.grey[50]
-                : theme.palette.grey[900],
         backgroundSize: "cover",
         backgroundPosition: "center",
-    },
-    paper: {
-        margin: theme.spacing(8, 4),
-        display: "flex",
-        flexDirection: "column",
+        marginTop: 0,
         alignItems: "center",
-        gap: 8,
+        justifyContent: "center",
     },
     avatar: {
         margin: theme.spacing(1),
         backgroundColor: theme.palette.secondary.main,
-        width: 54,
-        height: 54,
+        width: 150,
+        height: 150,
     },
     form: {
         width: "100%", // Fix IE 11 issue.
@@ -70,34 +57,32 @@ export default withAuthorization(
     };
 
     return (
-        <Grid container component="main" className={classes.root}>
-            <CssBaseline />
-            <Grid item xs={false} sm={4} md={7} className={classes.image} />
-            <Grid
-                container
-                xs={12}
-                sm={8}
-                md={5}
-                elevation={6}
-                square
-                alignItems="center"
-                justify="center"
-            >
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar} src={Logo} />
-                    <Typography component="h1" variant="h5" align="center">
-                        A portal for seeking covid related help
-                    </Typography>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={googleSignIn}
-                        style={{ marginTop: 8 }}
-                    >
-                        Login with smail
-                    </Button>
-                </div>
-            </Grid>
-        </Grid>
+        <div component="main" className={classes.root}>
+            {/* <CssBaseline /> */}
+            <div className="paper">
+                <Avatar className={classes.avatar} src={Logo} />
+                <Typography
+                    component="h1"
+                    variant="h5"
+                    align="center"
+                    style={{
+                        fontWeight: "bold",
+                        color: "white",
+                        marginTop: 10,
+                        marginBottom: 10,
+                    }}
+                >
+                    A portal for seeking covid related help
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={googleSignIn}
+                    style={{ marginTop: 8 }}
+                >
+                    Login with smail
+                </Button>
+            </div>
+        </div>
     );
 });
