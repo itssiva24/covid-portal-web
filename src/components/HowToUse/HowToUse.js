29d7a7a07d9a396f593958df2c991e198ab92af6
madhav-p <nanimadhav916@gmail.com>
2021-05-13 10:20:38 +0530

    added how to use page

---
+++ src/components/HowToUse/HowToUse.js
@@ -0,0 +1,85 @@
+import {
+    List,
+    ListItem,
+    ListItemText,
+    makeStyles,
+    Typography,
+} from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import { withAuthorization } from "../../contexts";
+
+const condition = (authUser) => !!authUser;
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        color: theme.palette.primary,
+    },
+}));
+
+export default withAuthorization(
+    condition,
+    ROUTES.SIGNIN
+)(() => {
+    const classes = useStyles();
+
+    return (
+        <>
+            <Typography
+                variant="h5"
+                style={{
+                    textAlign: "center",
+                    margin: 20,
+                }}
+            >
+                How To Use
+            </Typography>
+
+            <List component="ul">
+                <ListItem component="li">
+                    <ListItemText>
+                        <Typography component="span">
+                            1. All Requests will be visible on the{" "}
+                        </Typography>
+                        <Link className={classes.link} to={ROUTES.HOME}>
+                            Home Page
+                        </Link>
+                    </ListItemText>
+                </ListItem>
+                <ListItem component="li">
+                    <ListItemText>
+                        <Typography component="span">
+                            2. To make a new Request, go to{" "}
+                        </Typography>
+                        <Link className={classes.link} to={ROUTES.NEW_REQUEST}>
+                            New Request Page
+                        </Link>
+                        <Typography component="span">
+                            {" "}
+                            and Enter the necessary fields, upload necessary
+                            documents/images.
+                        </Typography>
+                    </ListItemText>
+                </ListItem>
+                <ListItem component="li">
+                    <ListItemText>
+                        <Typography component="span">
+                            3. Volunteers will be assigned for every request and
+                            they will provide all the necessary information and
+                            help with the process
+                        </Typography>
+                    </ListItemText>
+                </ListItem>
+                <ListItem component="li">
+                    <ListItemText>
+                        <Typography component="span">
+                            4. Once the request is resolved, it'll be be removed
+                            from the Requests page
+                        </Typography>
+                    </ListItemText>
+                </ListItem>
+            </List>
+        </>
+    );
+});
